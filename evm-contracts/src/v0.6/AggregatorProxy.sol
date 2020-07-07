b2eed118339b2cc9fa31d3252a1f843b1234ff5c
Steve Ellis <email@steveell.is>
2020-07-07 18:17:20 -0400

    introduce round aware aggregator proxy

--- evm-contracts/src/v0.6/AggregatorProxy.sol
+++ evm-contracts/src/v0.6/AggregatorProxy.sol
@@ -287,6 +287,7 @@ contract AggregatorProxy is AggregatorInterface, AggregatorV3Interface, Owned {
 
   function setAggregator(address _aggregator)
     internal
+    virtual
   {
     aggregator = AggregatorV3Interface(_aggregator);
   }
